d6d2a29d9a22ad331a4ab76fb2d562c702160c46
bouleetbil <bouleetbil@frogdev.info>
2010-08-20 23:39:06 +0000

    added some missing files oups

--- frugal-mono-tools/PacmanG2.cs
+++ frugal-mono-tools/PacmanG2.cs
@@ -4,12 +4,28 @@ namespace frugalmonotools
 {
 	public class PacmanG2
 	{
-		
+		private const string cch_pacmanconf ="/etc/pacman-g2.conf";
+		public string[] fwRepo;
+	
 		public PacmanG2 ()
 		{
-			pacman.pacman_initialize("/");
+			try{
+				pacman.pacman_initialize("/");
+			/*
+				if(pacman.pacman_parse_config(cch_pacmanconf,_db_cd,"")!=-1)
+				{
+				}
+			*/	  
+				
+			}
+			catch{}
 		}
-		
+				
+		private static void _db_cd(string  section,SWIGTYPE_p_f_p_q_const__char_p_struct___pmdb_t__void db)
+		{
+			Console.WriteLine(section);		
+		}
+				
 	}
 }
 
