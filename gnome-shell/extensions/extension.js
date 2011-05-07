eabe85e008fe323f7f491715bd77cd902622d540
bouleetbil <bouleetbil@frogdev.info>
2011-05-07 22:56:21 +0200

    *fix import

--- gnome-shell/extensions/extension.js
+++ gnome-shell/extensions/extension.js
@@ -21,6 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE. */
 
 const StatusIconDispatcher = imports.ui.statusIconDispatcher;
+const Panel = imports.ui.panel;
 
 function main()
 {
