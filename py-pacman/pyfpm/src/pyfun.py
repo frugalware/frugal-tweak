2b6316c9b7f237cabeba7dd3a3e62611d72154db
bouleetbil <bouleetbil@frogdev.info>
2012-01-10 12:25:08 +0100

    pyfun *part of pyfpm

---
+++ py-pacman/pyfpm/src/pyfun.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+#
+# Copyright (C) gaetan gourdin 2011 <bouleetbil@frogdev.info>
+# 
+# pyfpm is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# pyfpm is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from gi.repository import Gtk, GdkPixbuf, Gdk
+import os, sys
+import pacmang2.libpacman
+from pacmang2.libpacman import *
+from pyfpmtools.tools import *
+
+#Comment the first line and uncomment the second before installing
+#or making the tarball (alternatively, use project variables)
+UI_FILE = "/home/gaetan/tmpgit/frugal-tweak/py-pacman/pyfpm/src/pyfun.ui"
+#UI_FILE = "/usr/local/share/pyfpm/ui/pyfun.ui"
+
+pyconfig=configuration()
+suxcommande=pyconfig.Read('configuration','sux')
+if suxcommande=="":
+	suxcommande="gksu"
+	
+class GUI:
+	def __init__(self):
+		#Global
+		self.packageSelected=""
+		
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file(UI_FILE)
+		self.builder.connect_signals(self)
+		self.window = self.builder.get_object('window')
+		if(self.window):
+			self.window.connect("destroy",Gtk.main_quit)
+
+			self.window.set_title("PyFun : update packages")
+			self.window.set_size_request(400, 300)
+
+			self.liststore = Gtk.ListStore(str,str,str)
+			pacman_init()
+			pacman_init_database()
+			pacman_register_all_database()
+			pkgs =[]
+			pkgs = pacman_check_update()
+			for pkg in pkgs:
+				self.liststore.append([pacman_pkg_get_info(pkg,PM_PKG_NAME),pacman_pkg_get_info(pkg,PM_PKG_VERSION),pacman_pkg_get_info(pkg,PM_PKG_DESC)])
+			pacman_finally()
+
+			self.treeview =self.builder.get_object("treeview_pkg")
+			self.columnPkgname = Gtk.TreeViewColumn('Name')
+			self.columnDesc = Gtk.TreeViewColumn('Description')
+			self.columnVers = Gtk.TreeViewColumn('Version')
+
+			self.treeview.append_column(self.columnPkgname)
+			self.treeview.append_column(self.columnDesc)
+			self.treeview.append_column(self.columnVers)
+
+			self.cellName = Gtk.CellRendererText()
+			self.cellDesc = Gtk.CellRendererText()
+			self.cellVers = Gtk.CellRendererText()
+
+			self.columnPkgname.pack_start(self.cellName, True)
+			self.columnDesc.pack_start(self.cellDesc, True)
+			self.columnVers.pack_start(self.cellVers, True)
+
+			self.columnPkgname.add_attribute(self.cellName, 'text', 0)
+			self.columnDesc.add_attribute(self.cellDesc, 'text', 1)
+			self.columnVers.add_attribute(self.cellVers, 'text', 2)
+
+			# on autorise la recherche
+			self.treeview.set_search_column(0)
+			# on autorise la classement de la colonne
+			self.columnPkgname.set_sort_column_id(0)
+			self.treeview.set_model(self.liststore)
+		
+		self.window.show_all()
+
+	def destroy(window, self):
+		pypacman.pacman_finally()
+		Gtk.main_quit()
+		
+	def on_BTN_update_clicked(*args):
+		sysexec(suxcommande+" python "+PYFPM_INST+" updatesys")
+		
+def main():
+	app = GUI()
+	Gtk.main()
+		
+if __name__ == "__main__":
+    sys.exit(main())
