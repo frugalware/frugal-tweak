1396ce6c7912ac65053e8510ed39a4b227da4323
bouleetbil <bouleetbil@frogdev.info>
2012-01-10 13:29:10 +0100

    libpacman *change archive name

--- py-pacman/libpacman/Create_archive.sh
+++ py-pacman/libpacman/Create_archive.sh
@@ -2,8 +2,10 @@
 
 # For create the tar.gz release
 VERSION=0.0.1
+archive=python_libpacman-$VERSION.tar.gz
 mkdir -p releases
+rm releases/$archive
 tar -a -c -p --add-file=pacmang2/libpacman.py --add-file=README --add-file=setup.py \
-	--file=releases/libpacmanPython-$VERSION.tar.gz
+	--file=releases/$archive
 cd releases
-gpg -ba -u 20F55619 libpacmanPython-$VERSION.tar.gz
+gpg -ba -u 20F55619 $archive
