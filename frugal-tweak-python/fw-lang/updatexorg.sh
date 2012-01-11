987fdb533d317f4bca9e6e1944e17339f83753d6
bouleetbil <bouleetbil@frogdev.info>
2012-01-11 14:03:18 +0100

    fw-lang *fix date

--- frugal-tweak-python/fw-lang/updatexorg.sh
+++ frugal-tweak-python/fw-lang/updatexorg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #/*
-# * Copyright (c) 2010 gaetan gourdin
+# * Copyright (c) 2011 gaetan gourdin
 # *
 # * Permission is hereby granted, free of charge, to any person obtaining a copy
 # * of this software and associated documentation files (the "Software"), to deal
